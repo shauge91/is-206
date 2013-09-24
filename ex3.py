59d7dd41471c19de43747993987d59ca837eff3a
shauge91 <Sveinung@krs-eduroam-s-158-37-242-212.uia.no>
2013-09-24 13:07:13 +0200

    commit

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6 # Skriver teksten "Hens" og deretter svaret
+print "Roosters", 100 - 25 * 3 % 4 
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+
+print "Is it true that 3+2 <5-7?" #Skriver teksten "Is it true that 3+2 <5-7?"
+
+print 3+2<5-7
+#skriver deretter true eller false
+
+print "What is 3+2?", 3+2 #Skriver "What is 3+2" og deretter svaret 
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more." 
+
+print "Is it greater?", 5> -2 #Skriver "Is it greater?" OG deretter true false
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<= -2 
