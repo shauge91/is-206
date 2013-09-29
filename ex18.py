78553a61c138dbc4911ccbfda14befdd0a0018f9
shauge91 <Sveinung@Sveinungs-MacBook-Air.local>
2013-09-29 22:58:15 +0200

    add

--- ex18.py
+++ ex18.py
@@ -1,10 +1,10 @@
 # this one is like your scripts with argv
-def print_two(*args):
-    arg1, arg2 = args
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+def print_two(*args): # we tell python we want to make a function using def for define. And give it a name (print_two), and tells it we want asterisk args
+    arg1, arg2 = args #unpacks the arguments 
+    print "arg1: %r, arg2: %r" % (arg1, arg2) #print arguments out
 		
 # Ok, that *args is actually pointless, we can just do this
-def print_two_again(arg1, arg2):
+def print_two_again(arg1, arg2): #we can skip the whole unpacking args and just use the names we want
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # this just takes one argument
